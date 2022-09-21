191205456d966bf73bfbdd1ab4fc5df16deb8c0a
Llorens Martinez <zllorens@b0be837b3399.ant.amazon.com>
2022-09-21 18:20:31 +0200

    Correct special characters and some incorrect answers

--- WebScraping/WebScraping/spiders/EC2_faq.py
+++ WebScraping/WebScraping/spiders/EC2_faq.py
@@ -7,15 +7,29 @@ class EC2_faq(scrapy.Spider):
     def parse(self, response):
         response_aux = response.css('p')[1:]
         for i in range(0,len(response_aux) - 1,1):
-            if (response_aux[i].css('p').css('b::text').get()) != None:
+            if ((response_aux[i].css('p').css('b::text').get()) != None) and ((response_aux[i].css('p').css('b::text').get()[0])=='Q'):
                 answer = ''
                 question = response_aux[i].css('p').css('b::text').get()
                 while True:
                     i = i + 1
-                    answer = answer + response_aux[i].css('p').get() + '\n'
-                    if (response_aux[i + 1].css('p').css('b::text').get()) != None:
+                    answer = answer + response_aux[i].css('p').get() + ' '
+                    if ((response_aux[i + 1].css('p').css('b::text').get()) != None) and ((response_aux[i + 1].css('p').css('b::text').get()[0])=='Q'):
                         yield {
-                            'Question': question,
-                            'Answer': BeautifulSoup(answer, "lxml").text
+                            'Question': question
+                                        .replace('\u00a0',' ')
+                                        .replace('\u00ae','')
+                                        .replace('\u201c','\'')
+                                        .replace('\u201d','\'')
+                                        .replace('\u2013','-')
+                                        .replace('\u2018','-')
+                                        .replace('\u2019','\''),
+                            'Answer': (BeautifulSoup(answer, "lxml").text)
+                                        .replace('\u00a0',' ')
+                                        .replace('\u00ae','')
+                                        .replace('\u201c','\'')
+                                        .replace('\u201d','\'')
+                                        .replace('\u2013','-')
+                                        .replace('\u2018','-')
+                                        .replace('\u2019','\'')
                             }
                         break
